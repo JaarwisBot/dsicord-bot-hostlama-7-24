fef0f549cad4d318ff485ebca819128a4f9f2336
JaarwisBot <44170892+JaarwisBot@users.noreply.github.com>
2018-10-15 20:21:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -43,21 +43,6 @@ client.on('message', msg => {
   
   }
 	
-	 client.on("guildMemberAdd", member => {
-	  
-	  var channel = member.guild.channels.find("name","giren:white_check_mark:");
-	  if (!channel) return;
-	  
-	  var role = member.guild.roles.find("name", "YENİ ÜYE");
-	  if (!role) return;
-	  
-	  member.addRole(role);
-	  
-	  channel.send(member + " artık " + role + "rolü ile aramızda");
-	  
-	  member.send("Aramıza hoş geldin! Artık @YENİ ÜYE rolüne sahipsin!")
-  
-  
-});
+	
 
 client.login(process.env.Bot_Token);
